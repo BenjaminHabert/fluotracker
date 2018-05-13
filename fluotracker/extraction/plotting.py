5b88eacc558ef35b2a5126d54a968ede63416c49
benjaminhabert <bhabert@quantmetry.com>
2018-05-13 21:15:19 +0200

    feat: figure to plot all particle tracks

--- fluotracker/extraction/plotting.py
+++ fluotracker/extraction/plotting.py
@@ -1,4 +1,6 @@
-import numpy as np
+import trackpy as tp
+import pandas as pd
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
@@ -26,17 +28,59 @@ def plot_frames_from_movie(frames, name):
     fig, rows = plt.subplots(n_frames, 2, figsize=(10, 5 * n_frames))
 
     for i, frame, (ax1, ax2) in zip(indexes, frames_to_plot, rows):
-        im1 = ax1.imshow(frames[0], vmin=0, vmax=255)
+        im1 = ax1.imshow(frames[0])
         ax1_divider = make_axes_locatable(ax1)
         cax1 = ax1_divider.append_axes("right", size="7%", pad="2%")
         fig.colorbar(im1, cax=cax1)
         ax1.grid(False)
         ax1.set_title('Frame: {:d}'.format(i))
 
-        ax2.hist(frame.flatten(), bins=np.arange(0, 261, 10), log=True, color='darkblue', width=9)
-        ax2.set_xlim([0, 261])
+        ax2.hist(frame.flatten(), bins=50, log=True, color='darkblue', width=9)
         ax2.set_title('Intensity Histogram')
 
     fig.suptitle('Movie: ' + name)
     fig.tight_layout()
+    return fig
+
+
+def plot_tracks_infos(tracks, name, frame=None):
+    plt, sns = set_defaults()
+
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 8))
+    fig.suptitle(name)
+
+    if frame is not None:
+        im = ax1.imshow(frame)
+        fig.colorbar(im, ax=ax1)
+        ax1.grid(False)
+    tp.plot_traj(tracks, ax=ax1)
+    ax1.set_title('{:d} particle tracks'.format(tracks['particle'].nunique()))
+
+    particle_infos = (
+        tracks
+        .groupby('particle')
+        .apply(lambda df: pd.Series({
+            'min_frame': df['frame'].min(),
+            'max_frame': df['frame'].max(),
+            'n_frames': len(df),
+            'raw_mass': df['raw_mass'].median()
+        }))
+        .reset_index()
+    )
+
+    ax2.hist(particle_infos['n_frames'], bins=50, log=True)
+    ax2.set_xlabel('Number of frames')
+    ax2.set_ylabel('Number of particles')
+
+    ax3.barh(y=particle_infos['particle'],
+             width=particle_infos['n_frames'],
+             left=particle_infos['min_frame'],
+             height=4 if particle_infos['particle'].max() > 100 else 1)
+    ax3.set_xlabel('Frame index')
+    ax3.set_ylabel('Particle index')
+
+    ax4.loglog(particle_infos['n_frames'], particle_infos['raw_mass'], '.', alpha=0.5)
+    ax4.set_xlabel('Number of frames')
+    ax4.set_ylabel('median raw_mass')
+
     return fig
--- fluotracker/extraction/run_extraction.py
+++ fluotracker/extraction/run_extraction.py
@@ -12,9 +12,8 @@ from fluotracker.extraction import (
     filter,
     preprocessing,
     plotting,
+    postprocessing,
 )
-
-
 
 
 def run():
@@ -47,8 +46,9 @@ def extract_tracks_from_movie(folder, name):
     print('Extracting movie: ' + name)
     _convert_movie_to_frames(folder, name)
     _preprocess_frames(folder, name)
-    # _extract_tracks(folder, name)
-    # _filter_tracks(folder)
+    _extract_tracks(folder, name)
+    _filter_tracks(folder, name)
+    _rotate_tracks(folder, name)
     print('Extraction complete for movie: ' + name)
 
 
@@ -74,10 +74,10 @@ def _convert_movie_to_frames(folder, name):
     path = os.path.join(folder, 'movie.tif')
 
     frames = files.load_movie_frames(path)
-    fig = plotting.plot_frames_from_movie(frames, name)
-
     outpath = os.path.join(folder, 'frames.npy')
     np.save(outpath, frames)
+
+    fig = plotting.plot_frames_from_movie(frames, name)
     outpath = os.path.join(folder, 'frames.png')
     fig.savefig(outpath)
 
@@ -87,10 +87,10 @@ def _preprocess_frames(folder, name):
     frames = np.load(path)
 
     frames = preprocessing.apply_wavelet_filter(frames)
-    fig = plotting.plot_frames_from_movie(frames, name)
-
     outpath = os.path.join(folder, 'frames_preprocessed.npy')
     np.save(outpath, frames)
+
+    fig = plotting.plot_frames_from_movie(frames, name)
     outpath = os.path.join(folder, 'frames_preprocessed.png')
     fig.savefig(outpath)
 
@@ -102,20 +102,40 @@ def _extract_tracks(folder, name):
 
     tracks = extract.extract_tracks_from_frames(frames)
     tracks['movie'] = name
-
     outpath = os.path.join(folder, 'tracks.csv')
     files.save_dataframe(tracks, outpath)
 
+    fig = plotting.plot_tracks_infos(tracks, name, frames[0])
+    outpath = os.path.join(folder, 'tracks.png')
+    fig.savefig(outpath)
 
-def _filter_tracks(folder):
+
+def _filter_tracks(folder, name):
     print('Keeping only long tracks')
     path = os.path.join(folder, 'tracks.csv')
     tracks = files.load_dataframe(path)
 
     filtered = filter.remove_short_tracks(tracks)
-
     outpath = os.path.join(folder, 'tracks_filtered.csv')
     files.save_dataframe(filtered, outpath)
+
+    fig = plotting.plot_tracks_infos(filtered, name)
+    outpath = os.path.join(folder, 'tracks_filtered.png')
+    fig.savefig(outpath)
+
+
+def _rotate_tracks(folder, name):
+    print('Rotating tracks')
+    path = os.path.join(folder, 'tracks_filtered.csv')
+    tracks = files.load_dataframe(path)
+
+    rotated = postprocessing.add_rotated_coordinates(tracks)
+    outpath = os.path.join(folder, 'tracks_rotated.csv')
+    files.save_dataframe(rotated, outpath)
+
+    fig = plotting.plot_particles_coordinates(rotated, 'x_rotated', 'y_rotated', name)
+    outpath = os.path.join(folder, 'tracks_rotated.png')
+    fig.savefig(outpath)
 
 
 if __name__ == "__main__":
